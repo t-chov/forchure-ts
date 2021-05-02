dbcbb908c9d5c1049fb7fbb64485d78f3fdcba17
Kohei Tsuyuki <kotsuyuki@gmail.com>
2021-05-02 12:58:06 +0900

    add version on build script

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 readonly DENOPATH=${1:-"deno"}
+readonly VERSION=$(git tag | sort -r --version-sort | head -n1)
 
 readonly TARGETS=(
     "x86_64-unknown-linux-gnu"
@@ -11,5 +12,7 @@ readonly TARGETS=(
 
 for _target in ${TARGETS[@]}
 do
-    ${DENOPATH} compile --unstable --allow-net -o output/forchure.${_target} src/forchure.ts
+    ${DENOPATH} compile --unstable --lite --allow-net \
+        -o output/forchure.${VERSION}.${_target} \
+        src/forchure.ts
 done
